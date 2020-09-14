e6b06f783ef23c368d077e0398b19f322f11eb9b
Eilima <ericklima34@gmail.com>
2020-09-13 21:59:19 -0500

    Add infinite scroll

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-const MAX_STORIES = 500;
-const STORY_INCREMENT = 30;
+export const MAX_STORIES = 500;
+export const STORY_INCREMENT = 30;
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import { getStoryIds } from "../services/hackerNews-api";
 import { Story } from "../components/Story";
 import "../styles/StoriesContainer.css";
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
+  const { count } = useInfiniteScroll();
   useEffect(() => {
     getStoryIds().then((ids) => setStoryIds(ids));
-  }, []);
+  });
 
   return (
     <React.Fragment id="global-style" classname="stories-container-wrapper">
       <h1>News Stories</h1>
-      {storyIds.map((storyId) => (
+      {storyIds.slice(0, count).map((storyId) => (
         <Story key={storyId} storyId={storyId} />
       ))}
     </React.Fragment>
